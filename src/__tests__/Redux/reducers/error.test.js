f39939fdaa1c3e68a1c91df2be8cb1f82d87ffdf
St1llman <dm1korneev@gmail.com>
2019-12-15 13:50:53 +0300

    add error reducer tests

---
+++ src/__tests__/Redux/reducers/error.test.js
@@ -0,0 +1,44 @@
+import reducer from 'Redux/reducers/error';
+import * as actionNames from 'Constants/actionNames';
+
+const getFailureActionName = (actionName) => `${actionName}_FAILURE`;
+const getRequestActionName = (actionName) => `${actionName}_REQUEST`;
+
+describe('error reducer', () => {
+  test('should return the initial state', () => {
+    expect(reducer(undefined, {})).toStrictEqual(
+      {},
+    );
+  });
+
+  test('should not handle actions without REQUEST or FAILURE postfix', () => {
+    expect(
+      reducer({}, {
+        type: actionNames.ADD_CHATS,
+      }),
+    ).toStrictEqual(
+      {},
+    );
+  });
+
+  test('should handle GET_RATES_FAILURE', () => {
+    expect(
+      reducer({}, {
+        type: getFailureActionName(actionNames.GET_RATES),
+        payload: { error: { message: 'TEST_MESSAGE' } },
+      }),
+    ).toStrictEqual(
+      { GET_RATES: 'TEST_MESSAGE' },
+    );
+  });
+
+  test('should handle GET_RATES_REQUEST', () => {
+    expect(
+      reducer({ GET_RATES: 'TEST_MESSAGE' }, {
+        type: getRequestActionName(actionNames.GET_RATES),
+      }),
+    ).toStrictEqual(
+      { GET_RATES: '' },
+    );
+  });
+});
