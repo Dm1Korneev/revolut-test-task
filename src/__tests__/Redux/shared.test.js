2bad430168163020c781e87bbf090e859a6ae1eb
St1llman <dm1korneev@gmail.com>
2019-12-15 13:50:41 +0300

    add redux shared tests

---
+++ src/__tests__/Redux/shared.test.js
@@ -0,0 +1,45 @@
+import { getFailureAction, getRequestAction, getSuccessAction } from 'Redux/shared';
+
+describe('shared', () => {
+  test('should create get failure action', () => {
+    const actionName = 'ACTION_NAME';
+    const params = {
+      test: 'test',
+    };
+    const expectedResult = {
+      type: 'ACTION_NAME_FAILURE',
+      payload: {
+        test: 'test',
+      },
+    };
+    expect(getFailureAction(actionName, params)).toStrictEqual(expectedResult);
+  });
+
+  test('should create get request action', () => {
+    const actionName = 'ACTION_NAME';
+    const params = {
+      test: 'test',
+    };
+    const expectedResult = {
+      type: 'ACTION_NAME_REQUEST',
+      payload: {
+        test: 'test',
+      },
+    };
+    expect(getRequestAction(actionName, params)).toStrictEqual(expectedResult);
+  });
+
+  test('should create get success action', () => {
+    const actionName = 'ACTION_NAME';
+    const params = {
+      test: 'test',
+    };
+    const expectedResult = {
+      type: 'ACTION_NAME_SUCCESS',
+      payload: {
+        test: 'test',
+      },
+    };
+    expect(getSuccessAction(actionName, params)).toStrictEqual(expectedResult);
+  });
+});
