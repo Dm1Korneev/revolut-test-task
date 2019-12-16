6208734a5f0edb61ec9f04041fbb6f0fefd7a43d
Dmitriy Korneev <Dm1korneev@gmail.com>
2019-12-16 17:39:13 +0300

    add store test

---
+++ src/__tests__/Redux/store.test.js
@@ -0,0 +1,57 @@
+
+import storeFactory from 'Redux/store';
+import createSagaMiddleware from 'redux-saga';
+import * as Redux from 'redux';
+
+jest.mock('redux-saga');
+createSagaMiddleware.mockImplementation(() => ({
+  run: jest.fn(),
+}));
+
+jest.mock('redux', () => ({
+  applyMiddleware: jest.fn(),
+  createStore: jest.fn(() => ({ STORE: 'STORE' })),
+}));
+
+jest.mock('Redux/reducers', () => ({
+  default: jest.fn(),
+}));
+
+jest.mock('redux-devtools-extension', () => ({
+  composeWithDevTools: jest.fn(),
+}));
+
+jest.mock('Redux/sagas', () => ({ SAGA: 'SAGA' }));
+
+describe('Store', () => {
+  beforeEach(() => jest.clearAllMocks());
+
+  test('should return object', () => {
+    expect(storeFactory()).toStrictEqual({ STORE: 'STORE' });
+  });
+
+  test('should call createSagaMiddleware', () => {
+    storeFactory();
+    expect(createSagaMiddleware).toHaveBeenCalledTimes(1);
+  });
+
+  test('should call applyMiddleware', () => {
+    storeFactory();
+    expect(Redux.applyMiddleware).toHaveBeenCalledTimes(1);
+  });
+
+  test('should call createStore', () => {
+    storeFactory();
+    expect(Redux.createStore).toHaveBeenCalledTimes(1);
+  });
+
+  test('should run root saga', () => {
+    const run = jest.fn();
+    createSagaMiddleware.mockImplementationOnce(() => ({
+      run,
+    }));
+
+    storeFactory();
+    expect(run).toHaveBeenCalledWith({ SAGA: 'SAGA' });
+  });
+});
