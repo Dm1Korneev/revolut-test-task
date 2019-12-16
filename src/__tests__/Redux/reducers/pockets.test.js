b791c5a1ec115bf76e5221957abff1f44d583245
Dmitriy Korneev <Dm1korneev@gmail.com>
2019-12-16 17:41:04 +0300

    add pockets reducer tests

---
+++ src/__tests__/Redux/reducers/pockets.test.js
@@ -0,0 +1,33 @@
+import reducer from 'Redux/reducers/pockets';
+import * as actionNames from 'Constants/actionNames';
+
+const defaultStore = {};
+
+describe('pockets reducer', () => {
+  test('should return the initial state', () => {
+    expect(reducer(undefined, {})).toStrictEqual(
+      defaultStore,
+    );
+  });
+
+  test('should handle SET_POCKETS', () => {
+    const pockets = {
+      GBP: 110.56,
+      EUR: 213.00,
+      USD: 100000.01,
+    };
+
+    const expectedResult = {
+      GBP: 110.56,
+      EUR: 213.00,
+      USD: 100000.01,
+    };
+
+    expect(
+      reducer(defaultStore, {
+        type: actionNames.SET_POCKETS,
+        payload: { pockets },
+      }),
+    ).toStrictEqual(expectedResult);
+  });
+});
