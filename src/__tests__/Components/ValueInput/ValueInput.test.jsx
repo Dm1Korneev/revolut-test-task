5eb47af895fef06292f64072eccdeaca3bb8df05
Dmitriy Korneev <Dm1korneev@gmail.com>
2019-12-17 18:47:42 +0300

    add tests to ValueInput component

--- src/__tests__/Components/ValueInput/ValueInput.test.jsx
+++ src/__tests__/Components/ValueInput/ValueInput.test.jsx
@@ -16,9 +16,33 @@ describe('ValueInput component', () => {
     expect(asFragment()).toMatchSnapshot();
   });
 
+  test('renders component with empty value', () => {
+    const propsLocal = { ...props, value: null };
+    const { asFragment } = render(<ValueInput {...propsLocal} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test('renders component with 0 value', () => {
+    const propsLocal = { ...props, value: 0 };
+    const { asFragment } = render(<ValueInput {...propsLocal} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
   test('change value', () => {
     const { getByRole } = render(<ValueInput {...props} />);
     fireEvent.change(getByRole('textbox'), { target: { value: '200.01', valueAsNumber: 200.01 } });
     expect(changeValue).toHaveBeenCalledWith(200.01);
+  });
+
+  test('change value to null when input is clear', () => {
+    const { getByRole } = render(<ValueInput {...props} />);
+    fireEvent.change(getByRole('textbox'), { target: { value: '', valueAsNumber: Number.NaN } });
+    expect(changeValue).toHaveBeenCalledWith(null);
+  });
+
+  test('change value positive when inter negarive', () => {
+    const { getByRole } = render(<ValueInput {...props} />);
+    fireEvent.change(getByRole('textbox'), { target: { value: '-5', valueAsNumber: -5 } });
+    expect(changeValue).toHaveBeenCalledWith(5);
   });
 });
