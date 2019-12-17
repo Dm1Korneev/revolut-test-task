78ec6d06efe5b8b7054609058cc150cf870da820
Dmitriy Korneev <Dm1korneev@gmail.com>
2019-12-17 18:48:42 +0300

    add tests to Pocket component

---
+++ src/__tests__/Components/Pocket/Pocket.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Pocket from 'Components/Pocket';
+
+jest.mock('Components/CurrencySelect', () => global.mockComponent('CurrencySelect'));
+jest.mock('Components/ValueInput', () => global.mockComponent('ValueInput'));
+jest.mock('Components/Pocket/PocketValue', () => global.mockComponent('PocketValue'));
+
+describe('Pocket component', () => {
+  const props = {
+    currency: 'USD',
+    value: 100.87,
+    valueHave: 2000.90,
+    currencies: ['USD', 'EUR', 'GBP'],
+    setPocketCurrency: jest.fn(),
+    changeValue: jest.fn(),
+    valuePrefix: '-',
+    inputValueLabel: 'Test label input',
+    selectCurrencyLabel: 'Test label select',
+  };
+
+  test('renders the component', () => {
+    const { asFragment } = render(<Pocket {...props} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
