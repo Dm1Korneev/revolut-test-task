2abe2b00160590a27abfc4a6fa4871bd4fe25231
St1llman <dm1korneev@gmail.com>
2019-12-18 01:19:24 +0300

    add test to root saga

--- src/__tests__/Redux/sagas/index.test.js
+++ src/__tests__/Redux/sagas/index.test.js
@@ -1,19 +1,13 @@
 import rootSaga from 'Redux/sagas';
 import { all } from 'redux-saga/effects';
-import mainSaga from 'Redux/sagas/mainSaga';
 
-jest.mock('Redux/sagas/mainSaga', () => ({
-  * default() {
-    yield () => {};
-  },
-}));
+jest.mock('Redux/sagas/mainSaga', () => () => 'mainSaga');
 
 describe('Root saga', () => {
   const gen = rootSaga();
-
   test('should start main saga', () => {
     expect(gen.next().value).toStrictEqual(all([
-      mainSaga(),
+      'mainSaga',
     ]));
   });
 });
