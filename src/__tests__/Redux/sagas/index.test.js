a2f85ee69fecc707287efb82b716d0cf7b175c81
Dmitriy Korneev <Dm1korneev@gmail.com>
2019-12-18 15:30:08 +0300

    update root saga tests

--- src/__tests__/Redux/sagas/index.test.js
+++ src/__tests__/Redux/sagas/index.test.js
@@ -1,13 +1,106 @@
 import rootSaga from 'Redux/sagas';
-import { all } from 'redux-saga/effects';
+import * as getPocketsSaga from 'Redux/sagas/getPocketsSaga';
+import * as getRatesSaga from 'Redux/sagas/getRatesSaga';
+import * as changeWriteOffValueSaga from 'Redux/sagas/changeWriteOffValueSaga';
+import * as changeReceiveValueSaga from 'Redux/sagas/changeReceiveValueSaga';
+import * as exchangeSaga from 'Redux/sagas/exchangeSaga';
+import * as dropExchangeValuesSaga from 'Redux/sagas/dropExchangeValuesSaga';
 
-jest.mock('Redux/sagas/mainSaga', () => () => 'mainSaga');
+const spyGetPocketsSaga = jest.spyOn(getPocketsSaga, 'default').mockImplementation(() => () => {});
+const spyGetRatesSaga = jest.spyOn(getRatesSaga, 'default').mockImplementation(() => () => {});
+const spyChangeWriteOffValueSaga = jest.spyOn(changeWriteOffValueSaga, 'default').mockImplementation(() => () => {});
+const spyChangeReceiveValueSaga = jest.spyOn(changeReceiveValueSaga, 'default').mockImplementation(() => () => {});
+const spyExchangeSaga = jest.spyOn(exchangeSaga, 'default').mockImplementation(() => () => {});
+const spyDropExchangeValuesSaga = jest.spyOn(dropExchangeValuesSaga, 'default').mockImplementation(() => () => {});
 
 describe('Root saga', () => {
-  const gen = rootSaga();
-  test('should start main saga', () => {
-    expect(gen.next().value).toStrictEqual(all([
-      'mainSaga',
-    ]));
+  afterEach(() => {
+    spyGetPocketsSaga.mockClear();
+    spyGetRatesSaga.mockClear();
+    spyChangeWriteOffValueSaga.mockClear();
+    spyChangeReceiveValueSaga.mockClear();
+    spyExchangeSaga.mockClear();
+    spyDropExchangeValuesSaga.mockClear();
+  });
+
+  test('should take GET_POCKETS_REQUEST action', async () => {
+    const { emit } = await global.recordSaga(rootSaga);
+    emit({
+      type: 'GET_POCKETS_REQUEST',
+    });
+
+    expect(spyGetPocketsSaga).toHaveBeenCalledTimes(1);
+  });
+
+  test('should take GET_RATES_REQUEST action', async () => {
+    const { emit } = await global.recordSaga(rootSaga);
+    emit({
+      type: 'GET_RATES_REQUEST',
+    });
+
+    expect(spyGetRatesSaga).toHaveBeenCalledTimes(1);
+  });
+
+  test('should take CHANGE_WRITE_OFF_VALUE action', async () => {
+    const { emit } = await global.recordSaga(rootSaga);
+    emit({
+      type: 'CHANGE_WRITE_OFF_VALUE',
+    });
+
+    expect(spyChangeWriteOffValueSaga).toHaveBeenCalledTimes(1);
+  });
+
+  test('should take CHANGE_RECEIVE_VALUE action', async () => {
+    const { emit } = await global.recordSaga(rootSaga);
+    emit({
+      type: 'CHANGE_RECEIVE_VALUE',
+    });
+
+    expect(spyChangeReceiveValueSaga).toHaveBeenCalledTimes(1);
+  });
+
+  test('should take EXCHANGE_REQUEST action', async () => {
+    const { emit } = await global.recordSaga(rootSaga);
+    emit({
+      type: 'EXCHANGE_REQUEST',
+    });
+
+    expect(spyExchangeSaga).toHaveBeenCalledTimes(1);
+  });
+
+  test('should take EXCHANGE_SUCCESS action', async () => {
+    const { emit } = await global.recordSaga(rootSaga);
+    emit({
+      type: 'EXCHANGE_SUCCESS',
+    });
+
+    expect(spyDropExchangeValuesSaga).toHaveBeenCalledTimes(1);
+  });
+
+  test('should take SET_POCKET_FROM action', async () => {
+    const { emit } = await global.recordSaga(rootSaga);
+    emit({
+      type: 'SET_POCKET_FROM',
+    });
+
+    expect(spyDropExchangeValuesSaga).toHaveBeenCalledTimes(1);
+  });
+
+  test('should take SET_POCKET_TO action', async () => {
+    const { emit } = await global.recordSaga(rootSaga);
+    emit({
+      type: 'SET_POCKET_TO',
+    });
+
+    expect(spyDropExchangeValuesSaga).toHaveBeenCalledTimes(1);
+  });
+
+  test('should take CHANGE_POCKETS action', async () => {
+    const { emit } = await global.recordSaga(rootSaga);
+    emit({
+      type: 'CHANGE_POCKETS',
+    });
+
+    expect(spyDropExchangeValuesSaga).toHaveBeenCalledTimes(1);
   });
 });
