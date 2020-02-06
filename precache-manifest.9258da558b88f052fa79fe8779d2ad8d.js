e288986ab3c70c1353805e5ac53e82dcda76f673
Dm1Korneev <runner@fv-az51.4vq32gfwfoyepday1hphrwjx1c.cx.internal.cloudapp.net>
2020-02-06 12:43:02 +0000

    Updates

---
+++ precache-manifest.9258da558b88f052fa79fe8779d2ad8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa63dd334326be64061e7ca8d5b4bc21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b33c46375395e03b8ae0",
+    "url": "/static/js/2.10cbfda8.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.10cbfda8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3c87a0fa534c33239a0",
+    "url": "/static/js/main.9a1d2196.chunk.js"
+  },
+  {
+    "revision": "b5b7ec292e76cf171daa",
+    "url": "/static/js/runtime-main.804b6f3e.js"
+  }
+]);
